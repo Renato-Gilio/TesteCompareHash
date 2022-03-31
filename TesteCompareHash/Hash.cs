9fc79bdc6bed0051779ad1ff19d46e0b4a5532b4
Renato Augusto <blant_08@hotmail.com>
2022-03-31 09:58:55 -0300

    Add project files.

---
+++ TesteCompareHash/Hash.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TesteCompareHash
+{
+    public class Hash
+    {
+        public static string HashDeSenha(string senha, HashAlgorithm algoritmo)
+        {
+            var encodedValue = Encoding.UTF8.GetBytes(senha);
+            var encryptedPassword = algoritmo.ComputeHash(encodedValue);
+
+            var sb = new StringBuilder();
+            foreach (var caracter in encryptedPassword)
+            {
+                sb.Append(caracter.ToString("X2"));
+            }
+
+            return sb.ToString();
+        }
+
+        public static bool VerificarSenha(string senhaDigitada, string senhaCadastrada)
+        {
+            if (string.IsNullOrEmpty(senhaCadastrada))
+                throw new NullReferenceException("Cadastre uma senha.");
+
+            return senhaCadastrada == senhaDigitada ? true : false;
+        }
+    }
+
+}
+
---
+++ TesteCompareHash/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Security.Cryptography;
+
+
+namespace TesteCompareHash
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string digSenha = "Paralelpipedode";
+            Console.Write("Digite sua senha: ");
+            string senhaD   = Console.ReadLine();
+   
+            Console.WriteLine($"Senha: {digSenha}");
+            string senhaEncr = Hash.HashDeSenha(digSenha, MD5.Create());
+
+            //Validação
+            bool val = Hash.VerificarSenha(Hash.HashDeSenha(senhaD, MD5.Create()), senhaEncr);
+            string check = val ? "correta" : "errada";
+            Console.WriteLine($"Validação: {check}");
+
+            Console.WriteLine($"Hash1: {senhaEncr}");
+            Console.WriteLine($"Hash2: {Hash.HashDeSenha(senhaD, MD5.Create())}");
+            Console.ReadKey();
+        }
+    }
+}
